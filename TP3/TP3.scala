b188ffa12428d730e17e16e7744ad1c730b2e804
Mog <mogzor+git@gmail.com>
2012-10-01 19:47:57 +0200

    2nd question

--- TP3/TP3.scala
+++ TP3/TP3.scala
@@ -3,7 +3,12 @@ import scala.collection.immutable.{Map => IMap, Set => ISet}
 
 object TP3 {
   def main(args: Array[String]) {
+    println("Question 1:")
     println(toMap1("nantes") mkString "\n")
+    println
+    println("Question 2:")
+    println(toMap2("nantes") mkString "\n")
+    println
   }
   
   def toMap1(string: String): MMap[Char, MSet[Int]] = {
@@ -18,5 +23,24 @@ object TP3 {
     map
   }
   
+  def toMap2(string: String): IMap[Char, ISet[Int]] = {
+    def worker(
+      str: String,
+      map: IMap[Char, ISet[Int]],
+      index: Int)
+      : IMap[Char, ISet[Int]] = {
+      if(str.isEmpty)
+        map
+      else
+        worker(
+          str.tail,
+          map + (str.head -> (map get (str.head) match {
+            case Some(s) => s + index
+            case None    => ISet[Int](index)
+          })),
+          index + 1)
+    }
+    worker(string, IMap[Char, ISet[Int]](), 0)
+  }
 }
 
