f53ec743841ac336bfade6c0050cf6417366c810
Mog <mogzor+git@gmail.com>
2012-10-01 19:03:18 +0200

    fix exo 1

--- TP3/TP3.scala
+++ TP3/TP3.scala
@@ -3,14 +3,18 @@ import scala.collection.immutable.{Map => IMap, Set => ISet}
 
 object TP3 {
   def main(args: Array[String]) {
-    println(toMap1("nantes").mkString("\n"))
+    println(toMap1("nantes") mkString "\n")
   }
   
   def toMap1(string: String): MMap[Char, MSet[Int]] = {
     val map = MMap[Char, MSet[Int]]()
-    for(i <- 0 until string.length) {
-      map.getOrElse(string(i), MSet()).add(i)
-    }
+    for((c, i) <- string.zipWithIndex)
+      map get c match {
+        case Some(s) =>
+          s add i
+        case None    =>
+          map.put(c, MSet(i))
+      }
     map
   }
   
